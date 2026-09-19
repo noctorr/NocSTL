20918d9c52cf657b737eb020c8aafdf850c3ef24
noctor <129334069+noctorr@users.noreply.github.com>
2026-09-19 19:41:40 +0000

    update

--- test/multithreaded/ts_vector.cpp
+++ test/multithreaded/ts_vector.cpp
@@ -1,9 +1,14 @@
-#include ".../lib/Utils/threadsafe_containers.hpp"
+#include "../lib/Utils/threadsafe_containers.hpp"
 #include <iostream>
 
 int main(void) {
     stl::vector<int> x { 1, 2, 3 };
 
+    int dat[3] = {1, 4, 5};
+
+    stl::vector<int> boom(dat);
+
+    std::cout << boom.at(0) << std::endl;
     std::cout << x.at(2) << std::endl;
     return 0;
 }
